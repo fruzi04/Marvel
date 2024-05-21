99357d53dd1f95321d1161e050f58db4bf87ad54
fruzi04 <yakerich228@gmail.com>
2024-05-21 11:59:29 +0300

    error boundaries

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -34,11 +35,18 @@ class App extends Component {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    {this.state.showRandomChar ? <RandomChar/> : null}
-                    <button style={{marginTop: '15px'}} className="button button__main" onClick={this.toggleRandomChar}><div className="inner">Hide random character</div></button>
+                    <ErrorBoundary>
+                        {this.state.showRandomChar ? <RandomChar/> : null}
+                    </ErrorBoundary>
+                    {this.state.showRandomChar ? <button style={{marginTop: '15px'}} className="button button__main" onClick={this.toggleRandomChar}><div className="inner">Hide random character</div></button> :<button style={{marginTop: '15px'}} className="button button__main" onClick={this.toggleRandomChar}><div className="inner">Show random character</div></button>}
+                    
                     <div className="char__content">
-                        <CharList onCharSelected={this.onCharSelected}/>
-                        <CharInfo charId={this.state.selectedChar}/>
+                        <ErrorBoundary>
+                            <CharList onCharSelected={this.onCharSelected}/>
+                        </ErrorBoundary>
+                        <ErrorBoundary>
+                            <CharInfo charId={this.state.selectedChar}/>
+                        </ErrorBoundary>
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision"/>
                 </main>
