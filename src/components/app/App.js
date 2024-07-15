76e0e245e3adebae5be41fa1d5be1a5bc3e0f69f
fruzi04 <yakerich228@gmail.com>
2024-07-15 15:57:07 +0300

    pages, router-dom

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,55 +1,23 @@
-import { useState } from "react";
-import PropTypes from "prop-types";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import {MainPage, ComicsPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
     
-    const [selectedChar, setChar] = useState(null);
-    const [showRandomChar, setRandomChar] = useState(true);
-    
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-     
-    const toggleRandomChar = () => {
-        setRandomChar(!showRandomChar)
-    }
-
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                {/* <ErrorBoundary>
-                    {showRandomChar ? <RandomChar/> : null}
-                </ErrorBoundary>
-                {showRandomChar ? <button style={{marginTop: '15px'}} className="button button__main" onClick={toggleRandomChar}><div className="inner">Hide random character</div></button> :<button style={{marginTop: '15px'}} className="button button__main" onClick={toggleRandomChar}><div className="inner">Show random character</div></button>}
-                
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-
-                <ComicsList/>
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<MainPage/>}/>
+                        <Route path="/comics" element={<ComicsPage/>}/>
+                    </Routes>
+                </main>
+            </div>
+        </Router>
     )
-}
-
-CharList.protTypes = {
-    onCharSelected: PropTypes.func.isRequired
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import {createRoot} from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from './components/app/App';
 
 import './style/style.scss';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+ReactDOM 
+    .createRoot(document.getElementById('root'))
+    .render(
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>   
+    )
