85690bb91a1e7ec4def95683c556f9f58d753ea8
fruzi04 <yakerich228@gmail.com>
2024-08-01 13:27:15 +0300

    ReactHelmet

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,9 +1,18 @@
+import { Helmet } from "react-helmet";
+
 import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner";
 
 const ComicsPage = () => {
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Marvel information portal"
+                />
+                <title>Marvel comics</title>
+            </Helmet>
             <AppBanner/>
             <ComicsList/>
         </>
--- src/components/pages/singleCharLayout/singleCharLayout.js
+++ src/components/pages/singleCharLayout/singleCharLayout.js
@@ -1,4 +1,8 @@
+import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
+
 import './singleCharLayout.scss';
+
 
 const SingleCharacterLayout = ({data}) => {
 
@@ -6,10 +10,19 @@ const SingleCharacterLayout = ({data}) => {
 
     return (
         <div className="single-comic">
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`Information about marvel character ${name}`}
+                />
+                <title>{name}</title>
+            </Helmet>
             <img src={thumbnail} alt={name} className="single-comic__char-img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{name}</h2>
                 <p className="single-comic__descr">{description}</p>
+                <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '8rem'}}
+            to="/">Back to main page</Link>
             </div>
         </div>
     )
